745fa15274dc29814999c5a359e5b857aa356d94
soumikchandra-ai <shibmunchandra@gmail.com>
2026-05-26 02:13:28 +0530

    Add files via upload
    Added code for returns

---
+++ src/bayesforge/returns/daily_returns.py
@@ -0,0 +1,38 @@
+import numpy as np
+import pandas as pd
+import yfinance as yf
+from bayesforge.data.load import get_data
+
+def get_daily_return(
+    ticker:str,
+    horizon:str,
+    lookback:int
+):
+    """
+    Extract the daily return of the ticker symbol
+    according to the horizon and lookback provided
+    by the user.
+    """
+    
+    df=get_data(ticker,horizon,lookback)
+    close=df['Close']
+    
+    returns=close.pct_change()
+    
+    return returns
+
+def get_log_returns(
+    ticker:str,
+    horizon:str,
+    lookback:int
+):
+    """
+    Converting the returns to log returns
+    """
+    
+    df=get_data(ticker,horizon,lookback)
+    
+    log_return=np.log(df['Close']/df['Close'].shift(1))
+    
+    return log_return.dropna()
+    
